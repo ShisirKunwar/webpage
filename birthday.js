f27eee93582dd53a284dec216beb61999ce55356
Note Clicker <kunwarshishir38@gmail.com>
2026-09-04 13:38:07 +0545

    birthday suprise initialization

---
+++ birthday.js
@@ -0,0 +1,32 @@
+
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  console.log("🟢 User connected:", socket.id);
+
+  socket.on("birthdayYes", () => {
+    console.log("🎂 Birthday started by:", socket.id);
+
+    // Send to OTHER connected users
+    socket.broadcast.emit("birthdayStarted");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("🔴 User disconnected:", socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`🎂 Server running on port ${PORT}`);
+});
